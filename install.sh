bdaf9d820a13aa115ff6adb4d4d67167fd1b1a01
Adam <adam@nineswiss.com>
2022-10-17 15:03:42 +1100

    Window Rules

---
+++ install.sh
@@ -0,0 +1,16 @@
+sudo dnf install -y @base-x
+sudo dnf install -y nano
+sudo dnf install -y lxterminal
+sudo dnf install -y qt6-*
+sudo dnf install -y xrandr
+sudo dnf install -y xdotool
+sudo dnf install -y xprop
+sudo dnf install -y wmctrl
+sudo dnf install -y kwin
+sudo dnf install -y bamf-daemon
+
+
+# SETUP
+sudo mkdir -p  ~/.local/share/aurorae/themes/
+cp -r local/share/aurorae/themes/Canta-light ~/.local/share/aurorae/themes/
+
